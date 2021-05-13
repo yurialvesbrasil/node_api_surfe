8efd843cb967d53066f3013e00e7c6c0dcad0aae
yuriprodepa <yuriprodepa@gmail.com>
2021-05-13 15:18:18 -0300

    Primeiro teste passando

---
+++ test/globals.d.ts
@@ -0,0 +1,7 @@
+  
+declare namespace NodeJS {
+    interface Global {
+      //https://stackoverflow.com/a/51114250
+      testRequest: import("supertest").SuperTest<import("supertest").Test>;
+    }
+  }
